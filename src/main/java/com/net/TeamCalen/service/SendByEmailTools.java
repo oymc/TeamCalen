ec0a481087a04569c66dea453678340170e7fd89
oymc <2994780356@qq.com>
2019-04-15 19:23:29 +0800

    发送不成功则重发

--- src/main/java/com/net/TeamCalen/service/SendByEmailTools.java
+++ src/main/java/com/net/TeamCalen/service/SendByEmailTools.java
@@ -12,22 +12,25 @@ import com.net.TeamCalen.utils.EmailUtils;
 import lombok.extern.slf4j.Slf4j;
 
 @Service
-@EnableAspectJAutoProxy(exposeProxy = true)
-@Slf4j
+//@EnableAspectJAutoProxy(exposeProxy = true)
+//@Slf4j
 public class SendByEmailTools {
 	
 	@Autowired  
-    private JavaMailSender mailSender;  
-	@Async 
-    public boolean asyncSendEmail(String sender,String receiver,String title,String text){  
+    private JavaMailSender mailSender; 
+	/**异步方法
+     * 有@Async注解的方法，默认就是异步执行的，会在默认的线程池中执行，但是此方法不能在本类调用；启动类需添加直接开启异步执行@EnableAsync。
+     * */
+	@Async
+	public void asyncSendEmail(String sender,String receiver,String title,String text){  
 		try {
 			EmailUtils emailUtils=new EmailUtils(mailSender);
-			emailUtils.sendEmail(sender, receiver, title, text);
+			while(emailUtils.sendEmail(sender, receiver, title, text)==false) {
+				System.out.println("邮件发送错误，重新发送中");
+			}
 		}catch (Exception e) {
 			e.printStackTrace();
-			return false;
 		}
-		return true;
 		
     }  
 }
